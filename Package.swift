1e4baa8f53e67e98bf9232fd5ebc9517c16f3c3a
Nguyen Hai Dang <nhdang103@gmail.com>
2026-09-20 11:23:46 +0700

    Update Package.swift
    hotfix for xcode 27

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "EFCountingLabel",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v15),
     ],
     products: [
         .library(name: "EFCountingLabel", targets: ["EFCountingLabel"]),
